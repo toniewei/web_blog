58b26b20e73e2a953959ef17efd8a8f5db40be9d
weiqing1 <qing.wei@mail.utoronto.ca>
2019-05-06 09:06:35 -0400

    May 6th

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    time = models.DateTimeField(default=timezone.now())
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
 
-class home(request):
-    context = {}
-    render(request,home.html,context)
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
